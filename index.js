bfb218d5f937efaff74f199a436af8eaf0c596ff
deepak22 <deepakchauhan221997@gmail.com>
2020-12-06 01:42:40 +0530

    Express server

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const http =require('http');
+const morgan =require('morgan')
 
 const hostname = 'localhost';
 const port =3000;
 const app=express();
+app.use(morgan('dev')) 
+
+app.use(express.static(__dirname+'/public'))
+
 app.use((req,res,next)=>{
-    console.log(req.headers);
     res.statusCode =200;
     res.setHeader('Content-Type','text/html')
     res.end('<html><body><h1> Hello! Express Server</h1></body></html>')
@@ -19,3 +23,6 @@ server.listen(port,hostname,()=>{
 //app means our app will use express
 //createserver now takes this app we declared
 //expressis adding external fncality whihc http server will use
+//express.static alllows to server static html files from local
+// morgan is an express middleware used to log information
+//Morgan is used for logging request details//dev means development version
